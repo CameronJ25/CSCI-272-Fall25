04fba52bc3290ecf2980ad76977d278d36d59166
CameronJ25 <cameron.joseph-hallman@jjay.cuny.edu>
2025-09-03 10:33:37 -0400

    Add files via upload

---
+++ Week-1/Sep-3-2025-FunctionCalling.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+double average(int scores[], int size){
+	int sum = 0;
+	for (int i = 0; i < size; i++){
+		sum += scores[i];
+	}
+	
+	return (double)sum/size;
+}
+
+int main() {
+	int userInput;
+	cout << "Enter the size of an array: " << endl;
+	cin >> userInput;
+	int scores[userInput] = {90, 85, 70, 95, 100};
+	
+	cout << "Average = " << average(scores, userInput) << endl;
+	
+	return 0;
+}
